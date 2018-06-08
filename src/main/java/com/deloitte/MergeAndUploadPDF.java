cba3c7ca5aa2a31b2ff0091e13012085cacfc879
nagesingh@deloitte.com <nagesingh@deloitte.com>
2018-06-08 08:04:28 +0530

    Revert "User MappedByte buffer"
    This reverts commit 11c8235

--- src/main/java/com/deloitte/MergeAndUploadPDF.java
+++ src/main/java/com/deloitte/MergeAndUploadPDF.java
@@ -12,11 +12,8 @@ import com.sforce.ws.ConnectionException;
 import com.sforce.ws.ConnectorConfig;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.MappedByteBuffer;
-import java.nio.channels.FileChannel;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
@@ -90,6 +87,15 @@ public class MergeAndUploadPDF {
                             }
                         });
 
+                        //for (File inputFile : inputFiles) {
+                            /*PdfReader ReadInputPDF = new PdfReader(inputFile.toString());
+                            number_of_pages = ReadInputPDF.getNumberOfPages();
+                            for (int page = 0; page < number_of_pages; ) {
+                                copy.addPage(copy.getImportedPage(ReadInputPDF, ++page));
+                            }
+                            copy.freeReader(ReadInputPDF);
+                            ReadInputPDF.close();*/
+                        //}
                         PDFCombineUsingJava.close();
                         copy.close();
                         File mergedFile = new File("CombinedPDFDocument" + ".pdf");
@@ -98,13 +104,8 @@ public class MergeAndUploadPDF {
                         System.out.println("Creating ContentVersion record...");
                         ContentVersion[] record = new ContentVersion[1];
                         ContentVersion mergedContentData = new ContentVersion();
-                        final FileChannel channel = new FileInputStream(mergedFile.getName()).getChannel();
-                        MappedByteBuffer buffer = channel.map(FileChannel.MapMode.READ_ONLY, 0, channel.size());
 
-                        mergedContentData.setVersionData(buffer.array());
-
-                        channel.close();
-
+                        mergedContentData.setVersionData(Files.readAllBytes(mergedFile.toPath()));
                         mergedContentData.setFirstPublishLocationId(parentId);
                         mergedContentData.setTitle("Merged Document");
                         mergedContentData.setPathOnClient("/CombinedPDFDocument.pdf");
